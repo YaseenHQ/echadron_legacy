c25a6f5b8199cd9bc95ada90abe52ed55ab61c80
YaseenHQ <nohashtagplease@gmail.com>
2026-08-24 13:05:12 -0400

    fix(nix): update pnpmDeps hash for the xstate and immer additions

--- flake.nix
+++ flake.nix
@@ -164,7 +164,7 @@
               inherit (finalAttrs) pname version src pnpmWorkspaces;
               inherit pnpm;
               fetcherVersion = 3;
-              hash = "sha256-k8hb4Xvtv4dsmkP3SNzgwiMcsDx2yS926uLwCxMqqtw=";
+              hash = "sha256-gKFgfWaFyr+/re9+8gwFnIKqgGoGZSbA8aVTRmXCEgQ=";
             };
 
             nativeBuildInputs = [
